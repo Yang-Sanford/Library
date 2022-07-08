603e5f6d5adcc2591f9020c3a6c005cb52a7b1b8
SanfordYang <hsiangyang@mirle.com.tw>
2022-07-08 09:27:32 +0800

    NLog Tool And Method Library

---
+++ NLog/NLog/NLogBase.cs
@@ -0,0 +1,80 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+using System;
+
+namespace NLogCustomize
+{
+    public class NLogBase
+    {
+        #region [建構函式]
+        
+        /// <summary>
+        /// 建構式
+        /// </summary>
+        public NLogBase()
+        {
+            Configuration = new LoggingConfiguration();
+        }
+
+        #endregion
+
+        #region [屬性]
+
+        public LoggingConfiguration Configuration;
+
+        #endregion
+
+        #region [方法]
+
+        /// <summary>
+        /// NLog Setup TargetName
+        /// </summary>
+        /// <param name="pConfig"></param>
+        /// <param name="pFileNmae"></param>
+        /// <param name="pTargetName"></param>
+        /// <returns>Logger Manager。</returns>
+        public Logger NLogSetup(LoggingConfiguration pConfig, string pFileNmae, string pTargetName)
+        {
+            try
+            {
+                FileTarget TargetInfo = new FileTarget
+                {
+                    FileName = "${basedir}/Logs/${shortdate}/" + pFileNmae + "Logger.log",
+                    Layout = "${longdate} | ${level:uppercase=true}  | ${message}",                   
+                    KeepFileOpen = true
+                    
+                };
+                FileTarget TargetDebug = new FileTarget
+                {
+                    FileName = "${basedir}/Logs/${shortdate}/" + pFileNmae + "Logger.log",
+                    Layout = "${longdate} | ${level:uppercase=true} | ${message}",
+                    KeepFileOpen = true
+                };
+                FileTarget TargetError = new FileTarget
+                {
+                    FileName = "${basedir}/Logs/${shortdate}/" + pFileNmae + "Logger.log",
+                    Layout = "${longdate} | ${level:uppercase=true} | ${message} ${newline} ${stacktrace}",
+                    KeepFileOpen = true,
+                };
+
+                // [Rules]
+                pConfig.AddRuleForOneLevel(LogLevel.Info, TargetInfo, pTargetName);
+                pConfig.AddRuleForOneLevel(LogLevel.Warn, TargetInfo, pTargetName);
+                pConfig.AddRuleForOneLevel(LogLevel.Debug, TargetDebug, pTargetName);
+                pConfig.AddRuleForOneLevel(LogLevel.Trace, TargetDebug, pTargetName);
+                pConfig.AddRuleForOneLevel(LogLevel.Error, TargetError, pTargetName);
+
+                // [Setup]
+                LogManager.Configuration = pConfig;
+                return LogManager.GetLogger(pTargetName);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ NLog/NLog/NLogRecord.cs
@@ -0,0 +1,106 @@
+﻿using NLog;
+using System;
+
+namespace NLogCustomize
+{
+    public class NLogRecord
+    {
+        #region [建構函式]
+
+        public NLogRecord() { }
+
+        public NLogRecord(Logger log)
+        {
+            logger = log;
+        }
+
+        #endregion
+
+        #region [屬性]
+
+        public Logger logger;
+
+        #endregion
+
+        #region [方法]
+
+        /// <summary>
+        /// 紀錄處理步驟
+        /// </summary>
+        /// <param name="pStep"></param>
+        public void LogStepAction(int pStep)
+        {
+            switch (pStep)
+            {
+                case 1:
+                    logger.Info("1 - Get Last Record");
+                    break;
+                case 2:
+                    logger.Info("2 - Get Source DataSet");
+                    break;
+                case 3:
+                    logger.Info("3 - Begin Transcation");
+                    break;
+                case 4:
+                    logger.Info("4 - Process Data");
+                    break;
+                case 5:
+                    logger.Info("5 - Batch Data Import");
+                    break;
+                case 6:
+                    logger.Info("6 - Update Last Data Record");
+                    break;
+                case 7:
+                    logger.Info("7 - Logger Process Record");
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 紀錄前一次執行結果 (時間)
+        /// </summary>
+        /// <param name="pUpdateTime"></param>
+        public void LogLastResult(DateTime pUpdateTime)
+        {
+            logger.Info($"(Last UpdateTime - {pUpdateTime:yyyy-MM-dd HH:mm:ss.ffffff})");
+        }
+
+        /// <summary>
+        /// 紀錄前一次執行結果 (心跳 | 時間 | 編號)
+        /// </summary>
+        /// <param name="pHeartBeat"></param>
+        /// <param name="pUpdateTime"></param>
+        /// <param name="pUpdateID"></param>
+        public void LogLastResult(DateTime pHeartBeat, DateTime pUpdateTime, string pUpdateID)
+        {
+            logger.Info($"Last HeartBeat  - {pHeartBeat:yyyy-MM-dd HH:mm:ss.fff}");
+            logger.Info($"Last UpdateTime - {pUpdateTime:yyyy-MM-dd HH:mm:ss.fff}");
+            logger.Info($"Last UpdateID   - {pUpdateID:yyyy-MM-dd HH:mm:ss.fff}");
+        }
+
+        /// <summary>
+        /// 顯示當次執行紀錄
+        /// </summary>
+        /// <param name="pDataCnt"></param>
+        /// <param name="pInsertCnt"></param>
+        /// <param name="pUpdateCnt"></param>
+        /// <param name="pDeleteCnt"></param>
+        public void LogExecResult(int pDataCnt, int pInsertCnt, int pUpdateCnt, int pDeleteCnt)
+        {
+            logger.Info($"(Source Data Count：{pDataCnt})");
+            logger.Info($"(Insert Data Count：{pInsertCnt})");
+            logger.Info($"(Update Data Count：{pUpdateCnt})");
+            logger.Info($"(Delete Data Count：{pDeleteCnt})");
+        }
+
+        /// <summary>
+        /// 分隔線 (用於每一次執行紀錄)
+        /// </summary>
+        public void Demarcation(string pTableName)
+        {
+            logger.Trace($"-------------------- {pTableName} Data Process  --------------------");
+        }
+
+        #endregion
+    }
+}
